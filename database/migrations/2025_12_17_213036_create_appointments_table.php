56952796a3aed57174142b69591f5cd9b90f3d75
Zuzanna <zuzannaorzechowska4@gmail.com>
2025-12-17 22:37:41 +0100

    konfiguracja migracji, utworzenie nowych 5 tabel

---
+++ database/migrations/2025_12_17_213036_create_appointments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('users');
+            $table->foreignId('doctor_id')->constrained('users');
+            $table->foreignId('pet_id')->constrained();
+            $table->foreignId('service_id')->constrained();
+            $table->dateTime('appointment_date');
+            $table->enum('status', ['scheduled', 'completed', 'cancelled'])->default('scheduled');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
