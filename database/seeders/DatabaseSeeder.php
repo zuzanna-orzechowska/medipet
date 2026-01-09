9dd1bbf5dba7e6f0070f30cd3466fb7027af1b3d
Zuzanna <zuzannaorzechowska4@gmail.com>
2026-01-09 13:45:50 +0100

    dodanie na sztywno jednego lekarza i jednego klienta, zmiany w DatabaSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ use App\Models\Service;
 use App\Models\Appointment;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Collection;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
         $clientRole = Role::create(['name' => 'klient']);
         $services = Service::factory(5)->create();
 
+        //administrator
         User::factory()->create([
             'name' => 'Zuza Administrator',
             'email' => 'admin@vet.pl',
@@ -26,12 +28,37 @@ class DatabaseSeeder extends Seeder
             'role_id' => $adminRole->id,
         ]);
 
-        $doctors = User::factory(3)->create([
+        //doktorzy
+        $doctors = new Collection();
+        
+        $doctors->push(User::factory()->create([
+            'name' => 'Anna Nowak',
+            'email' => 'anna.nowak@vet.pl',
+            'password' => Hash::make('Lekarz666'),
+            'role_id' => $doctorRole->id,
+        ]));
+
+        $randomDoctors = User::factory(2)->create([
             'role_id' => $doctorRole->id
         ]);
 
-        User::factory(10)->create(['role_id' => $clientRole->id])->each(function ($client) use ($doctors, $services) {
-            
+        $doctors = $doctors->concat($randomDoctors);
+
+        //klienci
+        $fixedClient = User::factory()->create([
+            'name' => 'Jacek Kopec',
+            'email' => 'jacek.kopec@poczta.pl',
+            'password' => Hash::make('Klient111'),
+            'role_id' => $clientRole->id,
+        ]);
+
+        $randomClients = User::factory(9)->create([
+            'role_id' => $clientRole->id
+        ]);
+
+        $allClients = collect([$fixedClient])->concat($randomClients);
+
+        $allClients->each(function ($client) use ($doctors, $services) {
             $pets = Pet::factory(rand(1, 2))->create([
                 'user_id' => $client->id
             ]);
